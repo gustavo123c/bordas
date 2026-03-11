77ea498e4624ed937a2b2d4149b2fa92037a32b3
Gustavo Costa <168137183+gustavo123c@users.noreply.github.com>
2026-03-11 00:34:09 +0000

    finalizdo

--- bordas/app/page.js
+++ bordas/app/page.js
@@ -1,62 +1,26 @@
+import Link from "next/link";
+
 export default function Home() {
-
-  const estiloPagina = {
-    padding: "40px",
-    fontFamily: "Arial"
-  };
-
-  const estiloTitulo = {
-    marginBottom: "20px"
-  };
-
-  const estiloLista = {
-    listStyle: "none",
-    padding: "0"
-  };
 
   const estiloLink = {
     display: "block",
     marginBottom: "10px",
-    textDecoration: "none",
-    backgroundColor: "#4CAF50",
+    backgroundColor: "green",
     color: "white",
     padding: "10px",
-    width: "250px",
-    borderRadius: "5px"
+    textDecoration: "none",
+    width: "200px"
   };
 
   return (
-    <div style={estiloPagina}>
+    <div style={{ padding: "40px" }}>
 
-      <h1 style={estiloTitulo}>Atividades de React</h1>
+      <h1>Atividades</h1>
 
-      <ul style={estiloLista}>
-
-        <li>
-          <a href="/Pg210" style={estiloLink}>
-            Atividade Pg210
-          </a>
-        </li>
-
-        <li>
-          <a href="/Pg231" style={estiloLink}>
-            Atividade Pg231
-          </a>
-        </li>
-
-        <li>
-          <a href="/Pg246" style={estiloLink}>
-            Atividade Pg246
-          </a>
-        </li>
-
-        <li>
-          <a href="/Pg263" style={estiloLink}>
-            Atividade Pg263
-          </a>
-        </li>
-
-      </ul>
+      <Link href="/page210" style={estiloLink}>page210</Link>
+      <Link href="/page231" style={estiloLink}>page231</Link>
+      <Link href="/page246" style={estiloLink}>page246</Link>
+      <Link href="/page263" style={estiloLink}>page263</Link>
 
     </div>
   );
